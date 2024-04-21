ec6f821557ae7b643af8e5a3e741958f1eacbab6
dsbates25 <159731994+dsbates25@users.noreply.github.com>
2024-04-21 19:25:46 -0400

    Update views.py

--- bates_finalapp/todolist/views.py
+++ bates_finalapp/todolist/views.py
@@ -5,9 +5,9 @@ from django.shortcuts import render
 def index(request): 
     return render(request,"todolist/index.html")
 
-def testuserpage(request): 
+def testuser(request): 
     context = {}
-    return render(request,"todolist/testuserpage.html")
+    return render(request,"todolist/testuser.html")
 
 
 def login(request): 
